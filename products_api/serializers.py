390321794e05a38b27d9b5f5d10b5335cd988f1e
jatinder <jatindersingh93@gmail.com>
2023-07-08 13:05:07 -0500

    serialize sizes

--- products_api/serializers.py
+++ products_api/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 from .models import ProductsModel
+from utils.models import ProductSizes
 
 
 # Serialize Products API for CRUD calls
 class ProductsSerializer(serializers.ModelSerializer):
+    size = serializers.SerializerMethodField()
     class Meta:
         model = ProductsModel
         fields = '__all__'
+    
+    def get_size(self, instance):        
+        return ProductSizes(instance.size).name
