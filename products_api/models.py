28d9ca1ae43c8e8584d1202880d391f6c1c2d16e
jatinder <jatindersingh93@gmail.com>
2023-07-09 22:09:37 -0500

    optimize model

--- products_api/models.py
+++ products_api/models.py
@@ -4,7 +4,7 @@ from utils.models import ProductSizes
 # Define Schema to products model
 class ProductsModel(models.Model):
     id = models.BigAutoField(primary_key=True)
-    product_id = models.CharField(max_length=100)
+    product_id = models.CharField(max_length=100, unique=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
     colour = models.CharField(max_length=100, null=True, blank=True)
